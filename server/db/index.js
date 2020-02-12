081e8668805986927c51899b114ef3b71c9f94cb
Mark <m.bae@live.com>
2020-02-11 23:44:12 -0500

    You said you checked all your file paths. The fact that I had to change a bunch of them determined that was a lie.

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,10 @@
-const { Pass, Resort } = require('./models/models.js');
 const { db } = require('./connection.js');
+const { Pass, Resort } = require('./models/models.js');
 
 Pass.hasMany(Resort);
 Resort.belongsTo(Pass);
 
-module.export = {
+module.exports = {
   Pass,
   Resort,
   db,
