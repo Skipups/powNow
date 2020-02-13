40869770eaae9ec2e8d74d8f7ca696ae0586d39c
Skipups <svetlana.rovinsky@gmail.com>
2020-02-13 09:06:58 -0500

    css but can't check, webpack errror

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -10,10 +10,10 @@ const Home = props => {
   if (passes.length === 0) return <h3>No Pass</h3>;
   //to do: redo layout as columns due to picture availability
   return (
-    <div class="row">
+    <div class="column">
       {passes.map(uniquePass => {
         return (
-          <div class="column">
+          <div class="row">
             <Link to={`/${uniquePass.name}/${uniquePass.id}`}>
               <img styles="width:100%" alt="complex" src={uniquePass.image} />
             </Link>
